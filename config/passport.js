dbc1ba54c534c2cf3f60a75e6b3bf66391ef2484
Jayantha Bandara <wmjb07@gmail.com>
2020-08-11 18:07:35 +0900

    Enable login by facebook authentication

--- config/passport.js
+++ config/passport.js
@@ -8,42 +8,42 @@ const { propfind } = require('../routes');
 
 module.exports = function (passport) {
 
-    // passport.use(new FacebookStrategy({
-    //     clientID: process.env.FACEBOOK_CLIENT_ID,
-    //     clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
-    //     callbackURL: "/auth/facebook/callback",      
-    //     enableProof: true,
-    //     profileFields: ['id', 'displayName', 'name', 'gender', 'photos']        
-    //     //passReqToCallback: true
-    //   },
-    //   async (accessToken, refreshToken, profile, done) => {
-    //     // You have the access token here!
-    //     console.log(profile);
+    passport.use(new FacebookStrategy({
+        clientID: process.env.FACEBOOK_CLIENT_ID,
+        clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
+        callbackURL: "/auth/facebook/callback",      
+        enableProof: true,
+        profileFields: ['id', 'displayName', 'name', 'gender', 'photos']        
+        //passReqToCallback: true
+      },
+      async (accessToken, refreshToken, profile, done) => {
+        // You have the access token here!
+        console.log(profile);
 
-    //     const newUser = {
-    //         loginId : profile.googleId,
-    //         displayName : profile.displayName,
-    //         firstName : profile.name.givenName,
-    //         lastName : profile.name.familyName,
-    //         image : profile.photos ? profile.photos[0].value : '/img/faces/unknown-user-pic.jpg'
-    //     };
+        const newUser = {
+            Id : profile.googleId,
+            displayName : profile.displayName,
+            firstName : profile.name.givenName,
+            lastName : profile.name.familyName,
+            image : profile.photos ? profile.photos[0].value : '/img/faces/unknown-user-pic.jpg'
+        };
 
-    //     try {
-    //         let user = await User.findOne({Id : profile.id});
-    //            if(user)
-    //            {
-    //             done(null, user);
-    //            }
-    //            else
-    //            {
-    //             user = await User.create(newUser);
-    //             done(null, user);
-    //            }
-    //     } catch (err) {
-    //         console.error(err);
-    //       }
-    //   }
-    // ));
+        try {
+            let user = await User.findOne({Id : profile.id});
+               if(user)
+               {
+                done(null, user);
+               }
+               else
+               {
+                user = await User.create(newUser);
+                done(null, user);
+               }
+        } catch (err) {
+            console.error(err);
+          }
+      }
+    ));
 
     passport.use(new GoogleStrategy({
         clientID: process.env.GOOGLE_CLIENT_ID,
